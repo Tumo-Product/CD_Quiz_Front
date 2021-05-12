8cc2df085e055cca2fe7623c97cc7f5ddfdf25b4
HaykAvagyan1 <83754089+HaykAvagyan1@users.noreply.github.com>
2021-05-12 14:03:47 +0400

    Loading screen and temporary json parsing

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,34 @@
-let loaderHidden = false;
+let loaderHidden = true;
 
-$(function() {
-    if (loaderHidden) {     
+$(function () {
+    if (loaderHidden) {
         $("#loadingScreen").hide();
     } else {
         $("#loadingScreen").show();
+    }
+});
+
+async function getData() {
+    const response = await fetch("../test.json");
+    const json = await response.json();
+    return json;
+}
+
+getData().then(data => {
+    let sets = data.sets;
+
+    for (let section of sets) {
+        let name = section.name;
+        $("#name").html(name);
+
+        for (let question of section.questions) {
+            if (!Array.isArray(question)) {
+                if (typeof(question) !== "object") {
+                    $(".questions").append("<div><button>" + question + "</button></div>");
+                }
+            }
+        }
+
+        break;
     }
 });
