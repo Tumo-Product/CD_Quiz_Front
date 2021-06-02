2fe0ceb7d12b3f6076b0cd899798561291cfeed7
HaykAvagyan1 <hayk.avagyan@tumo.org>
2021-06-02 16:34:51 +0400

    New design and swiping functionality
    Cards swiping on screen.

--- scripts/flow.js
+++ scripts/flow.js
@@ -1,50 +1,85 @@
 const flow_data = {
-    set_data    : {},
-    index       : 0,
-    score       : 0,
-} 
-
-const onPageLoad = async () => {
-    let def = "Id not found";
-    let get_url =  document.location.href;
-    var url = new URL(get_url);
-    var _uid = url.searchParams.get("_uid");
-    if (_uid)
-    {
-        _uid = _uid.substr(1, _uid.length - 2);
-        let data = await parser.dataFetch(_uid);
-        flow_data.set_data = data.data.data;
-        view.drawStartingScreen(flow_data.set_data.name);
-        view.toggleLoadingScreen();
-    }
-    else
-        alert("_uid not found");
+	set_data: {},
+	index: 0,
+	score: 0,
 }
 
-const nextQuestion = () => {
-    document.getElementById("questions").innerHTML = "";
-    if (flow_data.index + 1 != flow_data.set_data.questions.length) {
-        implementQuestion(flow_data.set_data.questions[++flow_data.index]);
-    } else {
-        view.drawEndingScreen(flow_data.set_data.answer, flow_data.score);
-    }
+let progWidth = 300 - 60;
+let step = 0;
+
+const onPageLoad = async () =>
+{
+	let def = "Id not found";
+	let get_url = document.location.href;
+	let url = new URL(get_url);
+	let _uid = url.searchParams.get("_uid");
+
+	if (_uid)
+	{
+		_uid = _uid.substr(1, _uid.length - 2);
+		let data = await parser.dataFetch(_uid);
+		flow_data.set_data = data.data.data;
+
+		view.drawStartingScreen(flow_data.set_data.name, "start");
+
+		for (let i = 0; i < flow_data.set_data.questions.length; i++)
+		{
+			generateCards(flow_data.set_data.questions[i], i, true);
+		}
+
+		view.drawEndingScreen(flow_data.set_data.questions.length);
+
+		view.toggleLoadingScreen();
+	}
+	else
+		alert("_uid not found");
 }
 
-const implementQuestion = (question) => {
-    view.setQuestionName(question.text);
-    view.setButtons(question.answers);
+const nextQuestion = () =>
+{
+	if (flow_data.index + 1 <= flow_data.set_data.questions.length)
+	{
+		let i = ++flow_data.index;
+
+		changeCard(flow_data.set_data.questions[i], i, true);
+
+		step = progWidth / flow_data.set_data.questions.length;
+		view.updateProgressBar(step);
+		
+		if (i ==  flow_data.set_data.questions.length) {
+			view.updateScore(flow_data.set_data.answer, flow_data.score, flow_data.set_data.questions.length);
+		}
+	}
 }
 
-const onButtonClick = (id) => {
-    view.changeColor(id, "grey");
-    flow_data.score += flow_data.set_data.questions[flow_data.index].answers[id].points;
-    nextQuestion();
+const generateCards = (question, i, rotate, right) =>
+{
+	view.createContainer("_" + i);
+	view.setQuestionName(question.text, i, rotate);
+	view.setButtons(question.answers, i);
 }
 
-const onPlay = () => {
-    view.hideElement("name");
-    view.hideElement("play");
-    implementQuestion(flow_data.set_data.questions[0]);
+const changeCard = (question, i, right) =>
+{
+	console.log(i);
+	view.swipe(i);
+
+	if (right)
+	{
+		$(`#_${i + 1}`).addClass("right");
+	}
+}
+
+const onButtonClick = (id) =>
+{
+	flow_data.score += flow_data.set_data.questions[flow_data.index].answers[id].points;
+	nextQuestion();
+}
+
+const onPlay = () =>
+{
+	// implementQuestion(flow_data.set_data.questions[0]);
+	view.onPLay(flow_data.set_data.questions.length);
 }
 
 $(onPageLoad())
--- scripts/view.js
+++ scripts/view.js
@@ -1,15 +1,38 @@
 const view = {
     loaderOpen: true,
-    setButtons: (list) => {
+	rotations: [3, -3],
+	name: '<div id="name"><h1></h1></div>',
+	questions: `<div id="questions">`,
+	progress: 0,
+
+	createContainer: (i) => {
+		let container =
+		`<div id="${i}" class="container">
+			<div id="questionName">
+				<h2></h2>
+			</div>
+			<div id="score">
+				<h2></h2>
+			</div>
+			</div>
+		</div>`;
+
+		$(".parent").eq(1).prepend(container);
+	},
+
+    setButtons: (list, i) => {
         let html = "";
         let id = 0;
+		
         for (let question of list) {
             html += view.createButton(question.text, id);
             id++;
         }
 
         $(function() {
-            $("#questions").append(html);
+			$(`#_${i}`).append(view.questions);
+
+            $(`#_${i} #questions`).append(html);
         })
     },
     toggleLoadingScreen: () => {
@@ -21,31 +44,74 @@ const view = {
             view.loaderOpen = true;
         }
     },
+	onPLay: (length) => {
+		$("#play").addClass("widen");
+
+		$("#start").addClass("left");
+		$(`#_1`).addClass("right");
+
+		for (let i = 0; i < length; i++) {
+			if (i > 2) {
+				$(`#_${i}`).addClass("offscreenRight");
+			}
+			
+			$(`#_${i}`).css("transform", "rotate(0deg)");
+		}
+
+		$("#_0").css("transform", "rotate(0deg)");
+
+		$("#play").attr("onclick", "").unbind("click");
+	},
     createButton: (val, id) => {
         return `<div><button onclick="onButtonClick(${id})" id ="${id}">` + val + "</button></div>";
     },
     changeColor: (id, color) => {
         document.getElementById(id).style.backgroundColor = color;
     },
-    setQuestionName: (qname) => {
-        $("#questionName").html(qname);
+    setQuestionName: (qname, i, rotate) => {
+        $(`#_${i} #questionName h2`).html(qname);
+
+		if (rotate) {
+			$(`#_${i}`).css("transform", `rotate(${view.rotations[i]}deg)`);
+		}
     },
-    drawStartingScreen: (name) => {
-        $("#name").html(name);
-        $("#name").after('<img id="play" onclick="onPlay()" src="graphics/play-fill.svg" alt="My Happy SVG"/>');
+	
+    drawStartingScreen: (name, i) => {
+		view.createContainer(i);
+
+        $(`#${i}`).append(view.name);
+
+        $(`#${i} #name h1`).html(name);
+        $("body").append('<div id="play" onclick="onPlay()"><div><img src="graphics/play-fill.svg" alt="My Happy SVG"/></div></div>');
     },
-    drawEndingScreen: (text, score) => {
-        view.hideElement("questionName");
-        $("#score").html(parser.finalScoreString(text, score));
+
+    drawEndingScreen: (i) => {
+		view.createContainer("_" + i);
+
+		$(`#_${i}`).addClass("offscreenRight");
+        
     },
-    hideElement: (id) => {
-        $(function() {
-            $("#" + id).hide();
-        });
-    },
-    showElement: (id) => {
-        $(function() {
-            $("#" + id).show();
-        });
-    }
+	updateScore: (text, score, i) => {
+		console.log(parser.finalScoreString(text, score));
+		$(`#_${i} #score h2`).html(parser.finalScoreString(text, score));
+	},
+	swipe: (i) => {
+		$("#_" + (i - 1)).removeClass("right left center offscreenRight");
+		$("#_" + (i + 1)).removeClass("right left center offscreenRight");
+		$("#_" + (i)).removeClass("right left center offscreenRight");
+
+		if (i == 1) {
+			$("#start").addClass("offscreen");
+		} else {
+			$("#_" + (i - 2)).addClass("offscreen");
+		}
+
+		$("#_" + i).addClass("center");
+		$("#_" + (i - 1)).addClass("left");
+	},
+	updateProgressBar: (step) => {
+		console.log(step);
+		view.progress += step;
+		$("#play div").css("margin-left", `${view.progress}px`);
+	}
 }
